1aaa8efc47572bc78eead4abb5061578b92b0a02
Özgür <ozguronsoy@gmail.com>
2026-09-10 05:06:46 +0300

    add thread context unit tests

--- src/renderers/thread_context.rs
+++ src/renderers/thread_context.rs
@@ -3,28 +3,38 @@ use std::{cell::Cell, thread::LocalKey};
 use crate::renderers::{RendererError, RendererResult};
 
 /// Dummy trait for selecting the optimal ThreadContextMask type.
+#[cfg(not(test))]
 pub trait MaskType {
     type Type;
 }
 /// Dummy struct for selecting the optimal ThreadContextMask type.
+#[cfg(not(test))]
 pub struct MaskSelector<const BITS: usize>;
+#[cfg(not(test))]
 impl MaskType for MaskSelector<128> {
     type Type = u128;
 }
+#[cfg(not(test))]
 impl MaskType for MaskSelector<64> {
     type Type = u64;
 }
+#[cfg(not(test))]
 impl MaskType for MaskSelector<32> {
     type Type = u32;
 }
+#[cfg(not(test))]
 impl MaskType for MaskSelector<16> {
     type Type = u16;
 }
+#[cfg(not(test))]
 impl MaskType for MaskSelector<8> {
     type Type = u8;
 }
 
 /// The type used as a bitmask to track thread context allocation states.
+#[cfg(test)]
+pub type ThreadContextMask = u8;
+#[cfg(not(test))]
 pub type ThreadContextMask = <MaskSelector<
     {
         if THREAD_CONTEXT_COUNT > 64 {
@@ -68,6 +78,7 @@ const MAIN_THREAD_CONTEXT_INDEX: usize = THREAD_CONTEXT_COUNT - 1;
 const MAIN_THREAD_CONTEXT_MASK_INDEX: usize =
     MAIN_THREAD_CONTEXT_INDEX / THREAD_CONTEXT_MASK_BIT_SIZE;
 // static asserts
+#[cfg(not(test))]
 const _: () = assert!(
     (THREAD_CONTEXT_COUNT > 64 && std::mem::size_of::<ThreadContextMask>() == 16)
         || (THREAD_CONTEXT_COUNT > 32 && std::mem::size_of::<ThreadContextMask>() == 8)
@@ -88,8 +99,8 @@ impl ThreadContextIndex {
     pub fn try_get(&self) -> RendererResult<usize> {
         let index = self.0.get();
         if index == INVALID_THREAD_CONTEXT_INDEX {
-            Err(RendererError::InvalidOperation(
-                "Current thread is not initialized.".to_string(),
+            Err(RendererError::invalid_operation(
+                "Current thread is not initialized.",
             ))
         } else {
             Ok(index)
@@ -166,4 +177,107 @@ pub fn unregister(
 /// Gets the maximum number of threads that can execute concurrently.
 pub const fn thread_context_count() -> usize {
     THREAD_CONTEXT_COUNT
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_thread_context_index() {
+        let index = ThreadContextIndex::new();
+        assert_eq!(index.0.get(), INVALID_THREAD_CONTEXT_INDEX);
+        let try_get = index.try_get();
+        assert!(try_get.is_err());
+        assert_eq!(try_get.err().unwrap(), RendererError::invalid_operation(""));
+    }
+
+    #[test]
+    fn test_register_main_thread() {
+        thread_local! {
+          static INDEX: ThreadContextIndex = const { ThreadContextIndex::new() };
+        }
+        let mut masks: ThreadContextMaskArray =
+            std::array::from_fn(|_| ThreadContextMask::default());
+
+        assert!(register(&INDEX, &mut masks, true).is_ok());
+        INDEX.with(|index| {
+            assert_eq!(index.0.get(), MAIN_THREAD_CONTEXT_INDEX);
+        });
+        assert_eq!(
+            masks[MAIN_THREAD_CONTEXT_MASK_INDEX],
+            1 << (MAIN_THREAD_CONTEXT_INDEX % THREAD_CONTEXT_MASK_BIT_SIZE)
+        );
+    }
+
+    #[test]
+    fn test_register_worker_thread() {
+        thread_local! {
+          static INDEX: ThreadContextIndex = const { ThreadContextIndex::new() };
+        }
+
+        let mut masks: ThreadContextMaskArray =
+            std::array::from_fn(|_| ThreadContextMask::default());
+        assert!(register(&INDEX, &mut masks, false).is_ok());
+        INDEX.with(|index| {
+            assert_ne!(index.0.get(), INVALID_THREAD_CONTEXT_INDEX);
+        });
+        assert_eq!(masks[0], 1);
+    }
+
+    #[test]
+    fn test_unregister() {
+        thread_local! {
+          static INDEX: ThreadContextIndex = const { ThreadContextIndex::new() };
+        }
+
+        let mut masks: ThreadContextMaskArray =
+            std::array::from_fn(|_| ThreadContextMask::default());
+        assert!(register(&INDEX, &mut masks, false).is_ok());
+        unregister(&INDEX, &mut masks);
+        INDEX.with(|index| {
+            assert_eq!(index.0.get(), INVALID_THREAD_CONTEXT_INDEX);
+        });
+        assert_eq!(masks[0], 0);
+    }
+
+    #[test]
+    fn test_multiple_masks() {
+        const { assert!(THREAD_CONTEXT_MASK_COUNT > 1) };
+
+        thread_local! {
+          static INDEX: ThreadContextIndex = const { ThreadContextIndex::new() };
+        }
+
+        let masks: std::sync::Mutex<ThreadContextMaskArray> =
+            std::sync::Mutex::new(std::array::from_fn(|_| ThreadContextMask::default()));
+        std::thread::scope(|s| {
+            let barrier = std::sync::Arc::new(std::sync::Barrier::new(THREAD_CONTEXT_COUNT + 1));
+            for i in 0..THREAD_CONTEXT_COUNT {
+                let barrier = barrier.clone();
+                let masks = &masks;
+                s.spawn(move || {
+                    let result = register(
+                        &INDEX,
+                        &mut masks.lock().unwrap(),
+                        i == MAIN_THREAD_CONTEXT_INDEX,
+                    );
+                    assert!(result.is_ok());
+                    barrier.wait();
+                    barrier.wait();
+                    unregister(&INDEX, &mut masks.lock().unwrap());
+                    barrier.wait();
+                });
+            }
+            barrier.wait();
+            for mask in *masks.lock().unwrap() {
+                assert_eq!(mask, ThreadContextMask::MAX);
+            }
+            barrier.wait();
+            barrier.wait();
+            for mask in *masks.lock().unwrap() {
+                assert_eq!(mask, 0);
+            }
+        });
+    }
 }
